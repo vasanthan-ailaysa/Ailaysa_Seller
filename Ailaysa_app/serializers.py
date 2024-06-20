6911da645f4f53aaf97d45fb34ee1d44332ecc33
faizal <faizal@ailaysa.net>
2024-06-20 13:39:25 +0530

    serializer

--- Ailaysa_app/serializers.py
+++ Ailaysa_app/serializers.py
@@ -1,9 +1,26 @@
 # Serializers.py
 from rest_framework import serializers
-from .models import Book
+from .models import Book ,Language , Genre , FormatType
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        feilds = '__all__'
+        fields = '__all__'
         
+class LanguageSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many = True, read_only =True)
+    class meta:
+        model = Language
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many = True, read_only =True)
+    class meta:
+        model = FormatType
+        fields = '__all__'
+        
+class FormatSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many = True, read_only =True)
+    class meta:
+        model = Genre
+        fields = '__all__'
