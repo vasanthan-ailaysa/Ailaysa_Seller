f14d04d96525c52ad210ccd5be1b3ea50b2a6c77
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-06-23 01:14:20 +0530

    change models

--- seller_auth/models.py
+++ seller_auth/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from seller_auth.Managers import UserManager
+from Ailaysa_app.models import Publisher
 
 
 class SellerUser(AbstractBaseUser, PermissionsMixin):
     username = None
-    name = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, null=False)
+    email = models.EmailField(max_length=100, unique=True, null=False)
+    publisher_id = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=False)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
