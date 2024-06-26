885ad96aa2faa0b3142b75809e4f151bff3af609
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-06-26 15:31:01 +0530

    seializer

--- seller_auth/serializers.py
+++ seller_auth/serializers.py
@@ -13,7 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        user = User.objects.create(email=validated_data['email'], name=validated_data['name'], publisher=validated_data['publisher'])
+        user = User.objects.create(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            publisher=validated_data['publisher']
+        )
         user.set_password(validated_data['password'])
         user.save()
         return user
