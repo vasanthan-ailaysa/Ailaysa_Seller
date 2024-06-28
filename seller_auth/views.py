e884a364bb8d15911d60369891fed26618a02d50
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-06-28 11:42:50 +0530

    pep8

--- seller_auth/views.py
+++ seller_auth/views.py
@@ -1,36 +1,35 @@
 from seller_auth.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_205_RESET_CONTENT, HTTP_400_BAD_REQUEST
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.exceptions import ValidationError
 
 
 class RegisterView(APIView):
     """
-    Registraion API View
+    Registration API View
     """
-    def post(self, request):
+    @staticmethod
+    def post(request):
         try:
             serializer = UserSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        except Exception as e:
-            return Response(status=HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class LogoutView(APIView):
     """
     Logout API View
     """
-
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        try:
-            token = RefreshToken(request.data['refresh'])
-            token.blacklist()
-            return Response(status=HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=HTTP_400_BAD_REQUEST)
+    @staticmethod
+    def post(request):
+        token = RefreshToken(request.data['refresh'])
+        token.blacklist()
+        return Response(status=status.HTTP_205_RESET_CONTENT)
