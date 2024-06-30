52a27076c675725568b2c1514ea8e072ffce575a
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-06-30 20:06:41 +0530

    pyaw

--- Ailaysa_Seller/settings.py
+++ Ailaysa_Seller/settings.py
@@ -170,11 +170,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/staticfiles/'
-MEDIA_URL = '/mediafiles/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
