c926c2683209d1182159c04847c1715f87beeb53
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-07-01 10:38:59 +0530

    loginpage

--- seller_auth/urls.py
+++ seller_auth/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='sign_up'),
-    path('', TokenObtainPairView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
