7e9cd04f71d5a6921ff78b099fb97c1e9f5063dd
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-07-01 10:38:59 +0530

    loginpage

--- Ailaysa_Seller/urls.py
+++ Ailaysa_Seller/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('seller_auth.urls')),
+    path('users/', include('seller_auth.urls')),
     path('api/', include('Ailaysa_app.urls')),
 ]
 
