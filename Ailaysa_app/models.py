7196e61b0081aec6ff40cf44d089896d80f704ca
vasanthan-ailaysa <vasanthan@ailaysa.net>
2024-07-01 12:07:45 +0530

    userlinking

--- Ailaysa_app/models.py
+++ Ailaysa_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from seller_auth.models import User
 
 # validate isbn13 min,max length
 isbn13_min_length_validator = MinLengthValidator(13, "ISBN-13 must be exactly 13 characters long.")
@@ -74,15 +75,16 @@ class Book(models.Model):
     price = models.IntegerField(default=0)
     author_bio = models.TextField(blank=True, null=True)
     unit_weight = models.IntegerField(null=True, blank=True)
-    title_in_original_language = models.CharField(max_length=50, null=True)
-    co_publisher = models.CharField(max_length=100, null=True)
+    title_in_original_language = models.CharField(max_length=50, null=True, blank=True)
+    co_publisher = models.CharField(max_length=100, null=True, blank=True)
     winner = models.TextField(null=True, blank=True)
-    country_of_publication = models.CharField(max_length=100, help_text="Country of publication", null=True)
+    country_of_publication = models.CharField(max_length=100, help_text="Country of publication", null=True, blank=True)
     designed_by = models.CharField(max_length=50, blank=True, null=True)
     edited_by = models.CharField(max_length=50, blank=True, null=True)
     translated_by = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     objects = models.Manager()
 
